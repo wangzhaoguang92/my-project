ae5dcaefd72b9dbda1b46449e0e52294cc2be309
wangzhaoguang <wangzhaoguang@hlchang.cn>
2018-05-18 14:33:06 +0800

    添加UI库

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from 'electron'
-
+import { app, BrowserWindow,ipcMain} from 'electron'
+const MacAdd=require('getmac');
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -20,8 +20,9 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
-  })
+    width: 1000,
+    backgroundColor:"#f60"
+  });
 
   mainWindow.loadURL(winURL)
 
@@ -29,6 +30,18 @@ function createWindow () {
     mainWindow = null
   })
 }
+
+//主进程向渲染进程发送异步消息把获取的mac地址传送给渲染进程
+ipcMain.on('synchronous-message', function (event, arg) {
+    MacAdd.getMac(function(err,macAddress){
+        if (err)  throw err;
+        let mac = macAddress; //获取mac地址
+        event.returnValue = mac;
+        // event.sender.send('asynchronous-reply', mac)
+    });
+
+});
+//获取机器mac地址
 
 app.on('ready', createWindow)
 
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,43 +1,43 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          该项目的目的，是为了要避免使用 vue 手动建立起 electron 应用程序。electron-vue 充分利用 vue-cli 作为脚手架工具，加上拥有 vue-loader 的 webpack、electron-packager 或是 electron-builder，以及一些最常用的插件，如vue-router、vuex 等等。
-        </span>
-        <system-information></system-information>
-      </div>
+    <el-row>
+      <el-button type="primary" @click="add()">主要按钮</el-button>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    </el-row>
   </div>
 </template>
 
 <script>
   import SystemInformation from './LandingPage/SystemInformation'
+  import {ipcRenderer } from 'electron'
 
   export default {
     name: 'landing-page',
     components: { SystemInformation },
+      data() {
+          return {
+              activeIndex: '1',
+              activeIndex2: '1'
+          };
+      },
     methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        },
+
+      add(){
+            //异步方法
+//          ipcRenderer.send('asynchronous-message', '我接到了');
+//          ipcRenderer.on('asynchronous-reply', function (event, arg) {
+//              console.log( `我获取到的mac地址: ${arg}`)
+//          })
+          const reply = ipcRenderer.sendSync('synchronous-message', '我接到了')
+          this.$message({
+              showClose: true,
+              message: `我获取到你的mac地址: ${reply}`,
+              type: 'success'
+          });
+
       }
     }
   }
@@ -61,9 +61,6 @@
         rgba(255, 255, 255, 1) 40%,
         rgba(229, 229, 229, .9) 100%
       );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
   }
 
   #logo {
